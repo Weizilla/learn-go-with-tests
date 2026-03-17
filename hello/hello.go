fc84ecd2e233cee3634f24ddff86ada48dc2868f
Wei Yang <wei@weizilla.com>
2026-03-16 22:40:47 -0700

    stuff

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,36 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello world"
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	prefix := greetingPrefix(language)
+
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world", "English"))
 }
